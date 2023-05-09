b09e4c37ad36698d0c2769b0fd103a9708843811
Jaren Koh <72970362+enviro500@users.noreply.github.com>
2023-05-09 17:31:25 +0800

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,22 +1,38 @@
 <?php
-// Verify the reCAPTCHA response
-$recaptcha_secret = 6LcD2PMlAAAAABEK36qRbTkCdg-3F5jDSDIg8fnW;
-$recaptcha_response = $_POST['g-recaptcha-response'];
-$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-$recaptcha_data = array(
-    'secret' => $recaptcha_secret,
-    'response' => $recaptcha_response
-);
-$recaptcha_options = array(
-    'http' => array (
-        'method' => 'POST',
-        'content' => http_build_query($recaptcha_data)
-    )
-);
-$recaptcha_context = stream_context_create($recaptcha_options);
-$recaptcha_result = file_get_contents($recaptcha_url, false, $recaptcha_context);
-$recaptcha_response_data = json_decode($recaptcha_result);
-
-// Handle the form submission
-if ($recaptcha_response_data->success) {
-    $promo_code
+    // check if the form has been submitted
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        // check if the recaptcha response has been received
+        if (isset($_POST['g-recaptcha-response'])) {
+            // your site secret key
+            $secret = "6LcD2PMlAAAAABEK36qRbTkCdg-3F5jDSDIg8fnW";
+            // get verify response data
+            $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+            $responseData = json_decode($verifyResponse);
+            
+            // if the response is valid
+            if ($responseData->success) {
+                // process the form data
+                $promoCode = $_POST["promo-code"];
+                
+                if ($promoCode === "ENVIRO500\$X48F9Z" || $promoCode === "toiletdooristheworst") {
+                    echo "Code Redeemed! https://bit.ly/3BlS71b";
+                } else {
+                    echo "Invalid Code";
+                }
+                
+            } else {
+                // if the response is invalid
+                echo "reCAPTCHA verification failed, please try again.";
+            }
+            
+        } else {
+            // if the recaptcha response has not been received
+            echo "reCAPTCHA verification failed, please try again.";
+        }
+        
+    } else {
+        // if the form has not been submitted
+        echo "Please submit the form first.";
+    }
+?>

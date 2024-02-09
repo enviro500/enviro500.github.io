741e58adfd87a5502260ea1be8309a2b6d3a2b9f
Jaren Koh <72970362+enviro500@users.noreply.github.com>
2024-02-09 09:24:30 +0800

    Create service-worker.js
    Signed-off-by: Jaren Koh <72970362+enviro500@users.noreply.github.com>

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// service-worker.js
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/redeem.html',
+        '/manifest.json',
+        '/icon.png',
+        // Add other assets to cache here
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
